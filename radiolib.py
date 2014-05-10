c07e3eb681eb447e21d1ccd26749fc588298449d
AideTechBot <mandio0302@gmail.com>
2014-05-10 19:27:58 -0300

    Made the listen function work
    Also commented out the dependency warnings

--- radiolib.py
+++ radiolib.py
@@ -21,7 +21,7 @@ class Radio:
 
 		status, result = commands.getstatusoutput("which rtl_fm")
 		if status > 0:
-			print "[WARNING] RLT_FM not found."
+			#print "[WARNING] RLT_FM not found."
 			self.RTLFM_installed = False
 
 		status, result = commands.getstatusoutput("which aplay")
@@ -30,7 +30,7 @@ class Radio:
 
 		status, result = commands.getstatusoutput("which ./PiFmDma")
 		if status > 0:
-			print "[WARNING] PiFmDma not found"
+			#print "[WARNING] PiFmDma not found"
 			self.PiFmDma_installed = False
 
 
@@ -47,7 +47,12 @@ class Radio:
 		call(["./PiFmDma", "send.wav", freq])
 
 	#listening on a frequency
-	def listen(self, freq):
+	def listen(self, freq, baud):
 		if not self.RTLFM_installed:
 			raise OSError("RTL_FM is not installed: Cannot listen")
+
 		#rtl_fm -f 103.3e6 -W -s 200000 -r 48000 - | aplay -r 48k -f S16_LE
+		self.rtlfm = Popen(["rtl_fm", "-f", str(freq) + "e6", "-W", "-s", "200000", "-r", "48000", "-"], stdout=PIPE)
+		self.aplay = Popen(["aplay", "-r", "48k", "-f", "S16_LE"], stdin=self.rtlfm.stdout, stdout=PIPE)
+
+		self.minimodem = Popen(["./minimodem", "--rx", "-8", "-q", baud], stdout=PIPE)
